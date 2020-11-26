e649f29ef3d3469a4dcc8deaf128d3a81d9d85b1
bertiepleass <75093164+bertiepleass@users.noreply.github.com>
2020-11-26 20:14:30 +0100

    Add files via upload

--- StrangeAttractor.py
+++ StrangeAttractor.py
@@ -12,29 +12,28 @@ C = -0.39595
 D = -4.67104
 
 rows = 2
-cols = 100
+n = 500000
 
-xy = np.zeros((rows, cols), dtype="f")
+xy = np.zeros((rows, n), dtype="f")
 # xy = zeroxy.astype("f")
 
 xy[1, 0] = 0
 xy[0, 0] = 0
 
 i = 1
-while i < 100:
+while i < n:
     xy[0, i] = math.sin(xy[1, i - 1] * A)-math.cos(xy[0,i-1] * B)
     xy[1, i] = math.sin(xy[0, i - 1] * C)-math.cos(xy[1,i-1] * D)
     #xy[0, i] = i/10
     #xy[1, i] = 2 * i/10
-    print("X = " + str(xy[0, i]) + "  Y = " + str(xy[1, i]))
-    print(xy[0,i])
-    print(xy.dtype)
+    #print("X = " + str(xy[0, i]) + "  Y = " + str(xy[1, i]))
+    #print(xy[0,i])
     i = i + 1
 
 
 
 
 
-plt.plot(xy[0, :], xy[1, :])
+plt.scatter(xy[0, :], xy[1, :], 0.0001)
 plt.ylabel('some numbers')
 plt.show()
